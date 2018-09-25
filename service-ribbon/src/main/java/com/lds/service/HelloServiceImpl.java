f45d8c8a4c75f7b813f56b7cdde3d47bb3430599
jasonSunhu <sunhu1@goldmantis.com>
2018-09-25 09:52:15 +0800

    在ribbon中使用断路器

--- service-ribbon/src/main/java/com/lds/service/HelloServiceImpl.java
+++ service-ribbon/src/main/java/com/lds/service/HelloServiceImpl.java
@@ -1,5 +1,6 @@
 package com.lds.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -11,8 +12,12 @@ import org.springframework.web.client.RestTemplate;
 public class HelloServiceImpl implements HelloService {
     @Autowired
     RestTemplate restTemplate;
-
+    @HystrixCommand(fallbackMethod = "hiError")//断路器相关注解
     public String hiService(String name) {
         return restTemplate.getForObject("http://client/home/"+name,String.class);
+    }
+    public String hiError(String name){
+        return "hi," + name + ",sorry,error";
+
     }
 }
